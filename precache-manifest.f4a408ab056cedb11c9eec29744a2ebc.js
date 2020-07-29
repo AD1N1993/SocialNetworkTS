d3811365dcb161b601608461a9732c62250df201
YuryGrivickiy <griv1993@gmail.com>
2020-07-29 11:13:08 +0300

    Updates

---
+++ precache-manifest.f4a408ab056cedb11c9eec29744a2ebc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64c7d2291291345d43b0e45273268888",
+    "url": "/SocialNetworkTS/index.html"
+  },
+  {
+    "revision": "16517d480dfd63239698",
+    "url": "/SocialNetworkTS/static/css/main.8f1738af.chunk.css"
+  },
+  {
+    "revision": "edb1f42e229b73721bf1",
+    "url": "/SocialNetworkTS/static/js/2.70fd705d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SocialNetworkTS/static/js/2.70fd705d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16517d480dfd63239698",
+    "url": "/SocialNetworkTS/static/js/main.8e85436f.chunk.js"
+  },
+  {
+    "revision": "7eb4c14507d8757d83d4",
+    "url": "/SocialNetworkTS/static/js/runtime-main.a21c5b04.js"
+  }
+]);
