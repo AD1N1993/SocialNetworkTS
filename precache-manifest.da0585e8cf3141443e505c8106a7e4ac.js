82d3e32597f3b2aa16cf09ee85b281aa8e47c15a
YuryGrivickiy <griv1993@gmail.com>
2020-12-04 14:56:47 +0300

    Updates

--- precache-manifest.da0585e8cf3141443e505c8106a7e4ac.js
+++ precache-manifest.da0585e8cf3141443e505c8106a7e4ac.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7bca22346f8edb1ec34b88ec00786ba9",
+    "revision": "60ce2c11d17702073d6397967c138c82",
     "url": "/SocialNetworkTS/index.html"
   },
   {
@@ -8,36 +8,36 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/SocialNetworkTS/static/css/3.5d12243b.chunk.css"
   },
   {
-    "revision": "e60b9839a8095985dd36",
+    "revision": "1c8edcb4482037e300d6",
     "url": "/SocialNetworkTS/static/css/4.4e146b5c.chunk.css"
   },
   {
-    "revision": "54ee79a1823145951a66",
+    "revision": "25849a5a6634b2358149",
     "url": "/SocialNetworkTS/static/css/main.39ec0406.chunk.css"
   },
   {
-    "revision": "c666bb7eab07cd5c5920",
-    "url": "/SocialNetworkTS/static/js/2.a895a464.chunk.js"
+    "revision": "c25c1b2660a277c81bd7",
+    "url": "/SocialNetworkTS/static/js/2.eab95b23.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/SocialNetworkTS/static/js/2.a895a464.chunk.js.LICENSE.txt"
+    "url": "/SocialNetworkTS/static/js/2.eab95b23.chunk.js.LICENSE.txt"
   },
   {
     "revision": "dc394f8a82ac7a4aee9e",
     "url": "/SocialNetworkTS/static/js/3.ecbccf24.chunk.js"
   },
   {
-    "revision": "e60b9839a8095985dd36",
-    "url": "/SocialNetworkTS/static/js/4.80856cbe.chunk.js"
+    "revision": "1c8edcb4482037e300d6",
+    "url": "/SocialNetworkTS/static/js/4.2598d006.chunk.js"
   },
   {
-    "revision": "54ee79a1823145951a66",
-    "url": "/SocialNetworkTS/static/js/main.2a668ccb.chunk.js"
+    "revision": "25849a5a6634b2358149",
+    "url": "/SocialNetworkTS/static/js/main.b581845f.chunk.js"
   },
   {
-    "revision": "6b9f604bbe2d99930580",
-    "url": "/SocialNetworkTS/static/js/runtime-main.e6c0f8c7.js"
+    "revision": "526767465dadc71b3df6",
+    "url": "/SocialNetworkTS/static/js/runtime-main.f1be76bd.js"
   },
   {
     "revision": "32a50f1cce789b66fb8549b8d4a96cc3",
